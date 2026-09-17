2c7b642d5a84fbd4c124c2d22d4f1571a7be7852
kimsjuju1-GonFreecss <kimsjuju1@gmail.com>
2026-09-17 13:36:14 +0900

    two files change

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,6 @@
 def is_prime_number(x):
+    if x <= 1:
+        print("Error : The input")
     for num in range(2, x):
         if x % num == 0:
             print("The number {num1} is not a prime number.".format(num1=x))
